38fbf17755fe777baf983946e39b46ee9c4e36a3
EmirBajramovic1 <emir.bajramovic@stu.ibu.edu.ba>
2025-12-26 22:54:44 +0100

    Milestone5 fix

--- backend/rest/index.php
+++ backend/rest/index.php
@@ -1,4 +1,16 @@
 <?php
+
+header("Access-Control-Allow-Origin: https://votingsystemfrontend-i59en.ondigitalocean.app");
+
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 require 'vendor/autoload.php';
 require_once 'config.php';
 
