00728a15b19a212a7d9a7298678a509fa905539a
EmirBajramovic1 <emir.bajramovic@stu.ibu.edu.ba>
2025-12-26 21:50:40 +0100

    Index.php change

--- backend/rest/index.php
+++ backend/rest/index.php
@@ -1,4 +1,12 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    http_response_code(200);
+    exit();
+}
 
 require 'vendor/autoload.php';
 require_once 'config.php';
